515ad0b617c5101285cf2ebc33c24e8a6dcd0a06
veroarriola <v.arriola@ciencias.unam.mx>
2026-01-29 19:59:40 -0600

    Transmite imágenes comprimidas y las muestra en la PC

--- pc/pcros_ws/src/camera_viewer/camera_viewer/viewer_node.py
+++ pc/pcros_ws/src/camera_viewer/camera_viewer/viewer_node.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+
 import cv2
 import numpy as np
 
@@ -10,7 +11,7 @@ class CameraViewer(Node):
         
         # Nos suscribimos al mismo tópico que creamos en la Raspberry
         self.subscription = self.create_subscription(
-            Image,
+            CompressedImage,  # Image
             '/camera/image',
             self.listener_callback,
             10)
@@ -18,23 +19,42 @@ class CameraViewer(Node):
         self.get_logger().info('Nodo visualizador iniciado. Esperando imágenes...')
 
     def listener_callback(self, msg):
-        # 1. Convertir el buffer de bytes del mensaje a un array de NumPy
-        # El tipo de dato es uint8 (unsigned int de 8 bits para colores 0-255)
-        im_arr = np.frombuffer(msg.data, dtype=np.uint8)
-        
-        # 2. Reestructurar el array plano a las dimensiones de la imagen (H, W, Canales)
-        # Usamos los datos que vienen en el propio mensaje
-        im_arr = im_arr.reshape((msg.height, msg.width, 3))
+        # # 1. Convertir el buffer de bytes del mensaje a un array de NumPy
+        # # El tipo de dato es uint8 (unsigned int de 8 bits para colores 0-255)
+        # im_arr = np.frombuffer(msg.data, dtype=np.uint8)
 
-        # 3. Conversión de Color: de RGB (ROS2) a BGR (OpenCV)
-        # Si no haces esto, los rojos se verán azules y viceversa
-        cv_image = cv2.cvtColor(im_arr, cv2.COLOR_RGB2BGR)
+        # # Intentamos el reshape usando la información del mensaje
+        # try:
+        #     # Si hay rayitas, a veces es porque el buffer trae más datos de los calculados
+        #     # Tomamos solo los bytes necesarios para (H * W * 3)
+        #     expected_size = msg.height * msg.width * 3
+            
+        #     # 2. Reestructurar el array plano a las dimensiones de la imagen (H, W, Canales)
+        #     # Usamos los datos que vienen en el propio mensaje
+        #     #im_arr = im_arr.reshape((msg.height, msg.width, 3))
+        #     im_arr = im_arr[:expected_size].reshape((msg.height, msg.width, 3))
 
-        # 4. Mostrar la imagen en una ventana
-        cv2.imshow("Stream de la Raspberry Pi", cv_image)
+        #     # 3. Conversión de Color: de RGB (ROS2) a BGR (OpenCV)
+        #     # Si no haces esto, los rojos se verán azules y viceversa
+        #     cv_image = cv2.cvtColor(im_arr, cv2.COLOR_RGB2BGR)
+
+        #     # 4. Mostrar la imagen en una ventana
+        #     cv2.imshow("Stream de la Raspberry Pi", cv_image)
+            
+        #     # Necesario para que la ventana de OpenCV se refresque
+        #     cv2.waitKey(1)
+        # except Exception as e:
+        #     self.get_logger().error(f'Error reconstruyendo imagen: {e}')
+
+        # Convertimos los bytes del JPEG a un array de numpy
+        np_arr = np.frombuffer(msg.data, np.uint8)
         
-        # Necesario para que la ventana de OpenCV se refresque
-        cv2.waitKey(1)
+        # Decodificamos el JPEG a una imagen BGR de OpenCV
+        cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        
+        if cv_image is not None:
+            cv2.imshow("Stream Comprimido", cv_image)
+            cv2.waitKey(1)
 
     def destroy_node(self):
         cv2.destroyAllWindows()
--- raspberry/ros_ws/src/rpy_camera/rpy_camera/camera_publisher.py
+++ raspberry/ros_ws/src/rpy_camera/rpy_camera/camera_publisher.py
@@ -3,12 +3,14 @@ from rclpy.node import Node
 
 import io
 from picamera2 import Picamera2
+import numpy as np
 from libcamera import controls
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 from threading import Condition
 
-from sensor_msgs.msg import Image
+#from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 
 
 class StreamingOutput(io.BufferedIOBase):
@@ -24,13 +26,15 @@ class StreamingOutput(io.BufferedIOBase):
 
 class CameraPublisher(Node):
     # Selected sensor format: 1536x864-SBGGR10_1X10 - Selected unicam format: 1536x864-pBAA
-    HEIGHT = 1536
-    WIDTH = 864
+    # Sensor: /base/soc/i2c0mux/i2c@1/imx708@1a - Selected sensor format: 2304x1296-SBGGR10_1X10 - Selected unicam format: 2304x1296-pBAA
+    HEIGHT = 1296 // 4 # 864
+    WIDTH = 2304 // 4 #1536
     def __init__(self):
         super().__init__('camera_publisher_node')
         
         # Editor que publica la imagen
-        self.publisher_ = self.create_publisher(Image, '/camera/image', 10)
+        #self.publisher_ = self.create_publisher(Image, '/camera/image', 10)
+        self.publisher_ = self.create_publisher(CompressedImage, '/camera/image', 10)
 
         # Configuración de PiCamera2
         self.picam2 = picam2 = Picamera2()
@@ -39,7 +43,8 @@ class CameraPublisher(Node):
         conf = picam2.create_video_configuration(
             main={
                 'format': 'RGB888',
-                'size': (CameraPublisher.HEIGHT, CameraPublisher.WIDTH)
+                #'size': (CameraPublisher.HEIGHT, CameraPublisher.WIDTH)
+                'size': (CameraPublisher.WIDTH, CameraPublisher.HEIGHT)
             }
         )
         picam2.configure(conf)
@@ -56,23 +61,49 @@ class CameraPublisher(Node):
 
     def timer_callback(self):
         # Capturamos un cuadro
-        frame = self.picam2.capture_array()
+        #frame = self.picam2.capture_array()
         #print(type(frame))
+        # Capturamos directamente en formato JPEG (muy rápido en el hardware de la Pi)
+        # if frame is not None:
+        #     # Elimina el relleno que agrega picamera2 para alinear 32 o 64 bytes
+        #     frame_contig = np.ascontiguousarray(frame)
 
-        if frame is not None:
-            msg = Image()
+        #     msg = Image()
+        #     msg.header.stamp = self.get_clock().now().to_msg()
+        #     #msg.header.frame_id = "camera_link"
+        #     msg.height = frame_contig.shape[0]  # CameraPublisher.HEIGHT
+        #     msg.width = frame_contig.shape[1] # CameraPublisher.WIDTH
+        #     msg.encoding = "rgb8"
+        #     msg.is_bigendian = 0
+        #     msg.step = frame_contig.shape[1] * 3  #CameraPublisher.WIDTH * 3    # Full row length in bytes = ancho * canales
+
+        #     # Convertimos el array de NumPy a bytes puros para el mensaje
+        #     msg.data = frame_contig.tobytes()
+
+        #     self.get_logger().info('Publicando imagen')
+        #     self.publisher_.publish(msg)
+
+
+        # Creamos un buffer en memoria RAM
+        stream = io.BytesIO()
+        
+        # Capturamos directamente en formato JPEG hacia el buffer
+        # Esto usa el encoder de hardware de la Pi
+        self.picam2.capture_file(stream, format='jpeg')
+        
+        if stream.tell() > 0:
+            msg = CompressedImage()
             msg.header.stamp = self.get_clock().now().to_msg()
-            #msg.header.frame_id = "camera_link"
-            msg.height = CameraPublisher.HEIGHT
-            msg.width = CameraPublisher.WIDTH
-            msg.encoding = "rgb8"
-            msg.is_bigendian = 0
-            msg.step = CameraPublisher.WIDTH * 3    # Full row length in bytes = ancho * canales
-
-            # Convertimos el array de NumPy a bytes puros para el mensaje
-            msg.data = frame.tobytes()
-
+            msg.header.frame_id = "camera_frame"
+            msg.format = "jpeg"
+            
+            # Obtenemos los bytes del buffer
+            msg.data = stream.getvalue()
+            
             self.publisher_.publish(msg)
+            
+        # Cerramos el buffer para liberar memoria
+        stream.close()
 
     def destroy_node(self):
         self.picam2.stop()
